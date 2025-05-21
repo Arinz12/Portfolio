085a419cf3798c9e507b31075ba665bb69dab652
Arinz12 <155432830+Arinz12@users.noreply.github.com>
2025-05-21 16:00:40 +0000

    Created AAB

---
+++ my-pwa/app/src/main/java/com/onrender/zonapay/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package com.onrender.zonapay.twa;
+
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
